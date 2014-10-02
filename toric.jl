eb7e3e4afa87add595486a3ba821553c794bfc8b
Marius Lewerenz <marius.lewerenz@gmail.com>
2014-10-02 12:18:10 +0200

    Some improvements, maybe.

--- toric.jl
+++ toric.jl
@@ -4,52 +4,63 @@ const L = 60
 
 plaquettes = trues((L, L))
 
-stabilizers = zeros(Bool, (L, L, 2*L, L))
-S = reshape(stabilizers, (L*L, 2*L*L))
+#SF = zeros(Bool, (2*2*L*L, 2*2*L*L))
+#
+#SF[:] = [
+#    zeros(Bool, (2*L*L, 2*L*L)) eye(2*L*L);
+#    eye(2*L*L) zeros(Bool, (2*L*L, 2*L*L))
+#]
+
+stabilizers = zeros(Bool, (L, L, 2, 2*L, L, 2))
+S = reshape(stabilizers, (2*L*L, 2*2*L*L))
 
 for i = 1:L
     for j = 1:L
-        stabilizers[i, j, 2*i, j] = 1
-        stabilizers[i, j, 2*i-1, j] = 1
-        stabilizers[i, j, 2*i, (j % L)+1] = 1
-        stabilizers[i, j, ((2*i) % (2*L))+1, j] = 1
-        #println("plaquette ($i, $j):")
-        #println((2*i, j))
-        #println((2*i-1, j))
-        #println((2*i, (j % L)+1))
-        #println((((2*i) % (2*L))+1, j))
+        # plaquettes
+        stabilizers[i, j, 1, 2*i, j, 2] = 1
+        stabilizers[i, j, 1, 2*i-1, j, 2] = 1
+        stabilizers[i, j, 1, 2*i, (j % L)+1, 2] = 1
+        stabilizers[i, j, 1, ((2*i) % (2*L))+1, j, 2] = 1
+        # stars
+        stabilizers[i, j, 2, (2*i%(2*L))+1, j, 1] = 1
+        stabilizers[i, j, 2, 2*i, j%L+1, 1] = 1
+        stabilizers[i, j, 2, (2*i%(2*L))+1, j%L+1, 1] = 1
+        stabilizers[i, j, 2, ((2*i+1)%(2*L))+1, j%L+1, 1] = 1
     end
 end
 
-logical_operators = zeros(Bool, (2, 2*L, L))
-LO = reshape(logical_operators, (2, 2*L*L))
+logical_operators = zeros(Bool, (2, 2*L, L, 2))
+LO = reshape(logical_operators, (2, 2*2*L*L))
 
 for i = 1:L
-    logical_operators[1, 1, i] = 1
+    logical_operators[1, 1, i, 2] = 1
+    logical_operators[2, 2*i, 1, 1] = 1
     #logical_operators[2, 2*i, 1] = 1
 end
 
-error = zeros(Uint, 2*L, L)
-E = reshape(error, (2*L*L,))
+error = zeros(Uint, 2*L, L, 2)
+E = reshape(error, (2*2*L*L,))
 
-V = S * E % 2
+#V = S * SF * E % 2
+V = S * circshift(E, (2*L*L,)) % 2
 
-result = reshape(V, (L, L))
+result = reshape(V, (L, L, 2))
 
-for n = 1:50
+for n = 1:100
     i, j = rand(Uint)%(2*L)+1, rand(Uint)%L+1
-    error[i, j] += 1
+    error[i, j, 1] += 1
     #error[:] = error % 2
-    V[:] = S * E % 2
+    #V[:] = S * circshift(E, (2*L*L,)) % 2
     #run(`clear`)
     #println("error at ($i, $j):")
     #println(result)
     #sleep(0.5)
 end
 
-println(result)
+V[:] = S * circshift(E, (2*L*L,)) % 2
+println(result[:,:,1])
 
-sy = apply((x,y,_)->[x y], findnz(result))
+sy = apply((x,y,_)->[x y], findnz(result[:,:,1]))
 print(sy[1,:])
 
 n = size(sy)[1]
@@ -84,40 +95,42 @@ for k in syndrome_list
     j = sy[k,2]
     if (abs(sy[k,1]-sy[l,1]) < L/2)
         for i = min(sy[k,1],sy[l,1]):max(sy[k,1],sy[l,1])-1
-            error[(2*i)+1, j] += 1
+            error[(2*i)+1, j, 1] += 1
         end
     else
         for i = 1:min(sy[k,1],sy[l,1])
-            error[(2*i)-1, j] += 1
+            error[(2*i)-1, j, 1] += 1
         end
         for i = max(sy[k,1],sy[l,1]):L-1
-            error[(2*i)+1, j] += 1
+            error[(2*i)+1, j, 1] += 1
         end
     end
     i = sy[l,1]
     if (abs(sy[k,2]-sy[l,2]) < L/2)
         for j = min(sy[k,2],sy[l,2])+1:max(sy[k,2],sy[l,2])
-            error[2*i, j] += 1
+            error[2*i, j, 1] += 1
         end
     else
         for j = 1:min(sy[k,2],sy[l,2])
-            error[2*i, j] += 1
+            error[2*i, j, 1] += 1
         end
         for j = max(sy[k,2],sy[l,2])+1:L
-            error[2*i, j] += 1
+            error[2*i, j, 1] += 1
         end
     end
 end
 
-#error[:] = error % 2
-V[:] = S * E % 2
+println(int(error))
 
-if findnz(result)[1] != []
-    println(result)
+#error[:] = error % 2
+V[:] = S * circshift(E, (2*L*L,)) % 2
+
+if findnz(result[:,:,1])[1] != []
+    println(result[:,:,1])
     println("error in correction procedure")
 end
 
-logical_error = LO * E % 2
+logical_error = LO * circshift(E, (2*L*L,)) % 2
 
 println(logical_error)
 
